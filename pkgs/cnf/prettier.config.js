41021a0bbaf067b07e2d7fa444ead05d68941374
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2023-03-09 14:17:27 +0800

    refactor: cnf pkg prettier

---
+++ pkgs/cnf/prettier.config.js
@@ -0,0 +1,12 @@
+// @ts-check
+"use strict";
+
+/** @type {import("prettier").Options} */
+const config = {
+    arrowParens: "avoid",
+    printWidth: 160,
+    tabWidth: 4,
+    trailingComma: "all",
+};
+
+module.exports = config;
